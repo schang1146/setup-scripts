b925a5065f89646b50b29383d800f18756b7f215
schang1146 <sammychang2185@gmail.com>
2022-10-12 21:25:11 -0700

    Add boilerplate

---
+++ setup_new_computer.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+VERSION="v0.0.1"
+#===============================================================================
+# title           setup-new-computer.sh
+# author          Sammy Chang
+#                 https://github.com/schang1146
+# thanks to       Joel Kesler
+#                 https://github.com/joelkesler
+#===============================================================================
+#   A shell script to help with the quick setup and installation of tools and 
+#   applications for new developers at Vendasta.
+# 
+#   Quick Instructions:
+#
+#   1. Make the script executable:
+#      chmod +x ./setup-new-computer.sh
+#
+#   2. Run the script:
+#      ./setup-new-computer.sh
+#
+#   3. Some installs will need your password
+#
+#   4. You will be promted to fill out your git email and name. 
+#      Use the email and name you use for Github
+#
+#   5. Follow the Post Installation Instructions in the Readme:
+#      "https://github.com/schang1146/setup-scripts#post-installation-instructions"
+#  
+#===============================================================================
