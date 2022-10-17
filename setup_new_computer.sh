d0c044b7837ac86c0bbafef4af94c3b87bd1e8bb
schang1146 <sammychang2185@gmail.com>
2022-10-16 19:13:42 -0700

    Update setup script for Ubuntu 22.04

--- setup_new_computer.sh
+++ setup_new_computer.sh
@@ -28,3 +28,59 @@ VERSION="v0.0.1"
 #      "https://github.com/schang1146/setup-scripts#post-installation-instructions"
 #  
 #===============================================================================
+
+# Ubuntu 22.04
+
+# update packages
+sudo apt -y update
+sudo apt -y upgrade
+
+# check if you have snap
+
+# download software with snap
+# git
+sudo apt -y install git-all
+# vscode
+sudo snap install --classic code
+# spotify
+sudo snap install spotify
+# bitwarden
+sudo snap install bitwarden
+# kdenlive
+sudo snap install kdenlive
+# slack
+sudo snap install slack
+# obsidian
+# v1.0.0 requires downloading the *.snap file from their website
+sudo snap install --dangerous --classic obsidian_1.0.0_amd64.snap
+# chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
+# discord
+sudo snap install discord
+# azure data studio
+# requires download *.deb file into current working directory
+sudo dpkg -i azuredatastudio-linux-<version string>.deb
+rm azuredatastudio-linux-<version string>.deb
+# obs studio
+sudo add-apt-repository ppa:obsproject/obs-studio
+sudo apt update
+sudo apt install ffmpeg obs-studio
+# vlc media player
+sudo snap install vlc
+# nvm
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+# pyenv
+sudo apt install curl # curl did not come standard with Ubuntu 22.04
+curl https://pyenv.run | bash
+echo '' >> ~/.bashrc
+echo '# pyenv setup' >> ~/.bashrc
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+echo '' >> ~/.profile
+echo '# pyenv setup' >> ~/.profile
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
+echo 'eval "$(pyenv init -)"' >> ~/.profile
